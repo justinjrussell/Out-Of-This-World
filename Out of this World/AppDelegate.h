677fb9c0162fc39b340621418beee1e0d9c7f094
Justin Russell <justinrussell@Justins-iMac.local>
2014-12-17 23:21:18 -0800

    Initial Commit

---
+++ Out of this World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of this World
+//
+//  Created by Justin Russell on 12/17/14.
+//  Copyright (c) 2014 Justin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
